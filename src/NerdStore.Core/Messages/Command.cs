2d7793008bfdabfb1894a4522fb33e09e8da6e55
GRMagic <gustavo.rm90@gmail.com>
2020-07-13 22:59:09 -0300

    Pedido - Adicionar Item Command

---
+++ src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        public ValidationResult ValidationResult { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract bool EhValido();
+    }
+}
---
+++ tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Válido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedido_ComandoEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.True(result);
+
+        }
+
+        [Fact(DisplayName = "Adicionar Item Command Inválido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_CommandoEstaInvalido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.Empty, Guid.Empty, "", 0, 0);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.Contains(AdicionarItemPedidoValidator.IdClienteErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidator.IdProdutoErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidator.NomeErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidator.QtdMinErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidator.ValorErroMsg, pedidoCommand.ValidationResult.Errors.Select(c => c.ErrorMessage));
+        }
+    }
+}
